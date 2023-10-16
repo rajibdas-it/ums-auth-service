b4c5889a4e47ede3bc3a3c41b87430aaa8bb652a
rajibdas-it <rajibrad@gmail.com>
2023-10-16 14:43:22 +0600

    commenting husky

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
-    message: "ums auth service is running",
-  });
-});
+    message: 'ums auth service is running',
+  })
+})
 
-export default app;
+export default app
