0d76a2f04167dbd3cc9b66d2eb2ba8ff7184de6a
rajibdas-it <rajibrad@gmail.com>
2023-10-31 15:06:43 +0600

    make optional in zod page

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -3,11 +3,13 @@ import { bloodGroup, gender } from '../../../constants/user';
 
 const updateStudentZodSchema = z.object({
   body: z.object({
-    name: z.object({
-      firstName: z.string().optional(),
-      middleName: z.string().optional(),
-      lastName: z.string().optional(),
-    }),
+    name: z
+      .object({
+        firstName: z.string().optional(),
+        middleName: z.string().optional(),
+        lastName: z.string().optional(),
+      })
+      .optional(),
     gender: z.enum([...gender] as [string, ...string[]]).optional(),
     dateOfBirth: z.string().optional(),
     email: z.string().email().optional(),
@@ -16,19 +18,23 @@ const updateStudentZodSchema = z.object({
     presentAddress: z.string().optional(),
     parmanentAddress: z.string().optional(),
     bloodGroup: z.enum([...bloodGroup] as [string, ...string[]]).optional(),
-    guardian: z.object({
-      fatherName: z.string().optional(),
-      fatherOccupation: z.string().optional(),
-      fatherContactNo: z.string().optional(),
-      motherName: z.string().optional(),
-      motherOccupation: z.string().optional(),
-      motherContactNo: z.string().optional(),
-    }),
-    localGuardian: z.object({
-      guardianName: z.string().optional(),
-      guardianOccupation: z.string().optional(),
-      guardianContactNo: z.string().optional(),
-    }),
+    guardian: z
+      .object({
+        fatherName: z.string().optional(),
+        fatherOccupation: z.string().optional(),
+        fatherContactNo: z.string().optional(),
+        motherName: z.string().optional(),
+        motherOccupation: z.string().optional(),
+        motherContactNo: z.string().optional(),
+      })
+      .optional(),
+    localGuardian: z
+      .object({
+        guardianName: z.string().optional(),
+        guardianOccupation: z.string().optional(),
+        guardianContactNo: z.string().optional(),
+      })
+      .optional(),
     profileImage: z.string().optional(),
     academicSemester: z.string().optional(),
     academicDepartment: z.string().optional(),
